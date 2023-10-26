6810552883f209bfbf5b32c1932871f26061fe10
Iltae Lee <iltae80@daum.net>
2023-10-26 17:38:14 -0600

    make it work for react 18

--- src/components/Editor/Emoji.js
+++ src/components/Editor/Emoji.js
@@ -1,18 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Picker } from "emoji-mart";
-import "emoji-mart/css/emoji-mart.css";
 
 const Emoji = ({ onPicked }) => {
   return (
-    <Picker
-      title=""
-      emoji="yum"
-      native={true}
-      onSelect={onPicked}
-      style={{ width: "365px" }}
-    />
+    <div>Emoji</div>
   );
 };
 
